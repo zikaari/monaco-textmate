e9e57f14ad95f27ec35ffe89de963ee939d46352
Pedro Lamas <pedrolamas@gmail.com>
2024-02-09 18:01:03 +0000

    Removes dependency to node "path" module

--- src/utils.ts
+++ src/utils.ts
@@ -43,6 +43,17 @@ export function mergeObjects(target: any, ...sources: any[]): any {
 	return target;
 }
 
+export function basename(path: string): string {
+	const idx = ~path.lastIndexOf('/') || ~path.lastIndexOf('\\');
+	if (idx === 0) {
+		return path;
+	} else if (~idx === path.length - 1) {
+		return basename(path.substring(0, path.length - 1));
+	} else {
+		return path.substr(~idx + 1);
+	}
+}
+
 let CAPTURING_REGEX_SOURCE = /\$(\d+)|\${(\d+):\/(downcase|upcase)}/;
 
 export class RegexSource {
